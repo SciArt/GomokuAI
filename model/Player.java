56b601250f5beacb7a6764c86600e73aa0611fff
SciArt <science.artificer@gmail.com>
2018-12-20 01:07:40 +0100

    Maven

--- model/Player.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public abstract class Player 
-{
-	public abstract Board.Position makeMove(Board b, Piece.Color c);
-	public abstract Board.Move makeMove(Board b, int black_count, int white_count);
-	public abstract boolean doPickColor(Board b);
-	public abstract Piece.Color pickColor(Board b);
-}
